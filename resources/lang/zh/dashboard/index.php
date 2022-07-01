ecf6c31fd5b07e827c848ca9ba5f5d22c8568226
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2022-07-01 18:35:48 +0800

    添加部分语言文件

---
+++ resources/lang/zh/dashboard/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'search' => '搜索服务器...',
+    'no_matches' => '没有找到与提供的搜索条件相匹配的服务器。',
+    'cpu_title' => 'CPU',
+    'memory_title' => '内存',
+];
---
+++ resources/lang/zh/passwords.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => '密码必须至少为六个字符并与确认信息一致。',
+    'reset' => '您的密码已重置！',
+    'sent' => '我们已将您的密码重置链接通过电子邮件发送给您！',
+    'token' => '此密码重置令牌无效。',
+    'user' => "请检查邮箱是否正确，我们无法找到该邮箱的使用者。",
+];
